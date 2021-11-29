f257413e02abbc5e6b4bfe3a6eceb2c93be2b039
tttttx2 <github@zeromail.ch>
2021-11-29 16:40:58 +0100

    added rsyslog for reverse proxy nginx

--- internal_CA/deploy.sh
+++ internal_CA/deploy.sh
@@ -5,12 +5,14 @@
 /usr/bin/cp certs/10.0.0.30.* ../mysql/ssl/ 
 /usr/bin/cp certs/10.0.0.40.* ../logging_store/ssl/
 /usr/bin/cp certs/10.0.0.41.* ../logging_view/ssl/
+/usr/bin/cp certs/10.0.0.42.* ../logging_rsyslog/ssl/
 /usr/bin/cp certs/10.0.0.50.* ../backup/ssl/
 
 /usr/bin/cp myCA.pem ../backup/imovies_internal.crt
 /usr/bin/cp myCA.pem ../core/imovies_internal.crt
 /usr/bin/cp myCA.pem ../logging_store/imovies_internal.crt
 /usr/bin/cp myCA.pem ../logging_view/imovies_internal.crt
+/usr/bin/cp myCA.pem ../logging_rsyslog/imovies_internal.crt
 /usr/bin/cp myCA.pem ../mysql/imovies_internal.crt
 /usr/bin/cp myCA.pem ../reverse_proxy/imovies_internal.crt
 /usr/bin/cp myCA.pem ../web/imovies_internal.crt
---
+++ logging_rsyslog/README.md
@@ -0,0 +1,4 @@
+https://www.tecmint.com/install-rsyslog-centralized-logging-in-centos-ubuntu/
+https://commandprompt.com/blog/docker-logging-with-rsyslog/
+
+mainly this: https://www.simulmedia.com/blog/centralized-docker-logging-with-rsyslog

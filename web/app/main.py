d49c37a34fa6efcc9f6e50decf6df6eda8a2b977
En-Yu Jenp <enyu.jenp@gmail.com>
2021-11-30 01:07:29 +0100

    added Bootstrap CSS and login via ID

--- web/app/main.py
+++ web/app/main.py
@@ -10,9 +10,14 @@ def index():
     status = None
     token = request.cookies.get('token')
     if token is not None:
-        print("token:",token)
         headers = {'X-SERVICE-NAME': os.getenv('SERVICE_NAME')}
         files = {'token': (None, token)}
+        r = requests.post('https://10.0.0.10/get_info',headers=headers,files=files)
+        if r.status_code is not 200:
+            resp = make_response(redirect('/login'))
+            resp.set_cookie('token', '', expires=0)
+            return resp
+        user_info = r.json()[0]
         if request.method == 'POST':
             if "create" in request.form:
                 r = requests.post('https://10.0.0.10/create_cert',headers=headers,files=files)
@@ -33,9 +38,10 @@ def index():
                 resp = make_response(redirect('/login'))
                 resp.set_cookie('token', '', expires=0)
                 return resp
-            else:
-                error = 'Invalid Operation!'
-        return render_template('index.html',status=status)
+            else: 
+                print(request.form)
+                status = 'Invalid Operation!'
+        return render_template('index.html',status=status,user_info=user_info)
     else:
         return redirect(url_for('login'))
 
@@ -46,7 +52,7 @@ def login():
     if request.method == 'POST':
         headers = {'X-SERVICE-NAME': os.getenv('SERVICE_NAME')}
         files = {
-            'email': (None, request.form['email']),
+            'uid': (None, request.form['uid']),
             'passwd': (None, request.form['passwd']),
         }
         r = requests.post('https://10.0.0.10/login',headers=headers,files=files)
